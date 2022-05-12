e9e49cc20b901e923c0939929d445f8adc541531
Rafał Zieliński <96017423+rafalz94@users.noreply.github.com>
2022-05-12 11:41:00 +0200

    Initial commit with packages and properties

---
+++ src/main/java/com/sda/transporeon/finalconferenceroom/FinalConferenceRoomApplication.java
@@ -0,0 +1,13 @@
+package com.sda.transporeon.finalconferenceroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalConferenceRoomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalConferenceRoomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sda/transporeon/finalconferenceroom/FinalConferenceRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sda.transporeon.finalconferenceroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalConferenceRoomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
