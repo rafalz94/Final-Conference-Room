196e40d12b7c82e0f799f56cb0b36354d2f73559
Wojciech <w.wasilewski@gmail.com>
2022-05-15 15:17:44 +0200

    added db repositories layer

---
+++ src/main/java/com/sda/transporeon/finalconferenceroom/conference_room/repository/ConferenceRoomRepository.java
@@ -0,0 +1,7 @@
+package com.sda.transporeon.finalconferenceroom.conference_room.repository;
+
+import com.sda.transporeon.finalconferenceroom.conference_room.model.ConferenceRoom;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ConferenceRoomRepository extends JpaRepository<ConferenceRoom, Integer> {
+}
---
+++ src/main/java/com/sda/transporeon/finalconferenceroom/organization/repository/OrganizationRepository.java
@@ -0,0 +1,7 @@
+package com.sda.transporeon.finalconferenceroom.organization.repository;
+
+import com.sda.transporeon.finalconferenceroom.organization.model.Organization;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrganizationRepository extends JpaRepository<Organization, Integer> {
+}
